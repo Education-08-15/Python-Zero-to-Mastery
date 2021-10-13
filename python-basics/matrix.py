29f9bafeda9b8afc4fef0411f4b9fe4cd76a773a
Shikshya Gautam <nanu.gautam2016@gmail.com>
2021-10-12 21:49:39 -0700

    matix

---
+++ python-basics/matrix.py
@@ -0,0 +1,23 @@
+# matrix 
+# used to represent 2D(multidimensional) elements
+#array inside array
+
+#2D matrix 
+matrix = [
+  [3,4],
+  [2,3],
+  [6,1]
+]
+
+print(matrix [2] [0]) # 6
+# 3D matrix
+matrix1 = [
+  [3,4],
+  [
+    [3,2],
+    [4,5]
+    ],
+  [6,1]
+]
+
+print(matrix1[1][1][0]) # 4
